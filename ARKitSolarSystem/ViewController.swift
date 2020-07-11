7e51a70f62f697b0506ebc332cc53769523e0352
Melikşah Dipçin <meliksahdipcin@gmail.com>
2020-07-11 20:49:36 +0300

    no message

--- ARKitSolarSystem/ViewController.swift
+++ ARKitSolarSystem/ViewController.swift
@@ -17,17 +17,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+                        
+        let world = createSphere(radius: 0.2, content: "world.png", vector: SCNVector3(0.5, 0.2, -1))
+        let mars = createSphere(radius: 0.15, content: "mars.png", vector: SCNVector3(1, 0.2, -1))
+        let venus = createSphere(radius: 0.1, content: "venus.png", vector: SCNVector3(0, 0.2, -1))
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.scene.rootNode.addChildNode(world)
+        sceneView.scene.rootNode.addChildNode(mars)
+        sceneView.scene.rootNode.addChildNode(venus)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.automaticallyUpdatesLighting = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+    }
+    
+    func createSphere(radius: CGFloat, content : String, vector : SCNVector3) -> SCNNode {
+        
+        let mySphere = SCNSphere(radius: radius)
+        let sphereMaterial = SCNMaterial()
+        
+        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/\(content)")
+        
+        mySphere.materials = [sphereMaterial]
+        
+        let node = SCNNode()
+        node.position = vector
+        node.geometry = mySphere
+        
+        return node
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,17 +64,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
